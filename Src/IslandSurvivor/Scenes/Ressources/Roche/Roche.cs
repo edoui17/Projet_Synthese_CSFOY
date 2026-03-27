dbf9604bfdd0a910908f1b71856bbb5c64aa1d9d
Mason <antoine_masson94@hotmail.com>
2026-03-27 13:46:00 -0400

    Creation de ma classe c# pour ma Roche. creation d'un signal lorsque la roche est briser qui sera envoyé avec la quantité de roche briser au signalManager. Ensuite, ajout de l'entité rock afin que pour les différents different type de ressource puissent être réutilisé avec les autres classes de ressources. Ensuite, géré lorsqu,on entre en contact avec la ressource. Validation si c'est un objet Tool. Ajout également d'un timer afin d'éviter de spammer la ressource. Dernièrement, fonction DestoyRock. Création d'un random qui va donner entre 1 et 4 ressource  qui sera envoyer en signal au gestionnaire de signal.

---
+++ Src/IslandSurvivor/Scenes/Ressources/Roche/Roche.cs
@@ -0,0 +1,40 @@
+using Godot;
+using System;
+
+public partial class Roche : Area2D
+{
+    [Signal] public delegate void RockBrokenEventHandler(int quantity);
+    [Export] public string EntityId = "rock";
+
+    private double _damageCooldown = 0.2;
+    private double _timerSinceLastDamage = 0.0;
+
+    public override void _Ready()
+    {
+        AreaEntered += OnAreaEntered;
+    }
+
+    private void OnAreaEntered(Area2D area)
+    {
+        if (_timerSinceLastDamage <= 0 && area.IsInGroup("Tool"))
+        {
+            StatManager.Instance.ApplyDamage(this, 1);
+            _timerSinceLastDamage = _damageCooldown;
+        }
+    }
+    public override void _Process(double delta)
+    {
+        if (_timerSinceLastDamage > 0) _timerSinceLastDamage -= delta;
+    }
+
+
+
+    public void DestroyRock()
+    {
+        Random random = new();
+        int quantity = random.Next(1, 5);
+
+        EmitSignal(SignalName.RockBroken, quantity);
+        QueueFree();
+    }
+}
