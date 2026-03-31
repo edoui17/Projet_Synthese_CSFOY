6882ae4583590e51613d064165ead7a864627d16
Édouard Couture <edouardcouture04@gmail.com>
2026-03-30 20:02:40 -0400

    correction du Y sorting de la map

--- Src/IslandSurvivor/Scenes/BaseMap/Spawn.cs
+++ Src/IslandSurvivor/Scenes/BaseMap/Spawn.cs
@@ -5,17 +5,35 @@ public partial class Spawn : Marker2D
 {
     // Called when the node enters the scene tree for the first time.
     [Export] public Marker2D SpawnPoint { get; set; }
-    [Export] public CharacterBody2d Player { get; set; }
+    [Export] public PackedScene PlayerScene { get; set; }
 
     public override void _Ready()
 	{
-        // var player = Instantiate<CharacterBody2d>(PlayerScene);
-        Player.Position = SpawnPoint.Position;
-        AddChild(Player);
+        if (PlayerScene == null)
+        {
+            GD.PrintErr("PlayerScene n'est pas assigné !");
+            return;
+        }
+
+        if (SpawnPoint == null)
+        {
+            GD.PrintErr("SpawnPoint n'est pas assigné !");
+            return;
+        }
+
+ 
+        CallDeferred(nameof(SpawnPlayer));
+    }
+    private void SpawnPlayer()
+    {
+        var player = PlayerScene.Instantiate<Node2D>();
+        player.Position = SpawnPoint.Position;
+
+        GetTree().CurrentScene.AddChild(player);
     }
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
 	{
 	}
 }
