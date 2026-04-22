daa069e3e3829d77cdbfc6fe162bae35ccfdc654
Kevin Houle <2496808@csfoy.ca>
2026-04-22 12:32:37 -0400

    correction merge conflict avant debogage  avec jules

--- Src/IslandSurvivor/Globals/ServiceRegistry.cs
+++ Src/IslandSurvivor/Globals/ServiceRegistry.cs
@@ -10,35 +10,36 @@ namespace IslandSurvivor.Globals;
 
 public partial class ServiceRegistry : Node
 {
-    public static ServiceRegistry Instance { get; private set; }
+  public static ServiceRegistry Instance { get; private set; }
 
-    public IInventoryManager InventoryManager { get; private set; }
-    public IShopManager ShopManager { get; private set; }
-    public IStatTracker StatTracker { get; private set; }
-    public IScoreTracker ScoreTracker { get; private set; }
-    public INavigationService NavigationService { get; private set; }
-    public ISignalManager SignalManagerCore { get; private set; }
+  public IInventoryManager InventoryManager { get; private set; }
+  public IShopManager ShopManager { get; private set; }
+  public IStatTracker StatTracker { get; private set; }
+  public IScoreTracker ScoreTracker { get; private set; }
+  public INavigationService NavigationService { get; private set; }
+  public ISignalManager SignalManagerCore { get; private set; }
 
-    public override void _EnterTree()
+  public override void _EnterTree()
+  {
+    if (Instance != null)
     {
-        if (Instance != null)
-        {
-            QueueFree();
-            return;
-        }
-
-        Instance = this;
-
-        // Initialize Core Managers
-        SignalManagerCore = new SignalManagerCore();
-        InventoryManager = new InventoryManager();
-        ShopManager = new ShopManager();
-        StatTracker = new StatTracker();
-
-        // Pass Godot specific implementation of ISaveService
-        ISaveService saveService = new GodotSaveService();
-        ScoreTracker = new ScoreTracker(saveService);
-
-        NavigationService = new NavigationService(SignalManagerCore);
+      QueueFree();
+      return;
     }
+
+    Instance = this;
+
+    // Initialize Core Managers
+
+    SignalManagerCore = new SignalManagerCore();
+    InventoryManager = new InventoryManager();
+    ShopManager = new ShopManager();
+    StatTracker = new StatTracker();
+
+    // Pass Godot specific implementation of ISaveService
+    ISaveService saveService = new GodotSaveService();
+    ScoreTracker = new ScoreTracker(saveService);
+    NavigationService = new NavigationService(SignalManagerCore);
+
+  }
 }
