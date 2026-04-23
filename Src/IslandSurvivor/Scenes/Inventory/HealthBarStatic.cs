0b1f7d89cab0d9ff1ee7a3e5fb6fb9fada2ffb5a
Delphine Martin <delphine.dm11@gmail.com>
2026-04-23 08:31:50 -0400

    Modif/Implémentation changement de vie selon des levels

---
+++ Src/IslandSurvivor/Scenes/Inventory/HealthBarStatic.cs
@@ -0,0 +1,56 @@
+using Godot;
+using System;
+
+[Tool]
+public partial class HealthBarStatic : Control
+{
+    private int _level = 0;
+
+    [Export]
+    public int Level
+    {
+        get => _level;
+        set
+        {
+            _level = value;
+            UpdateSize();
+        }
+    }
+
+    [Export] public TextureProgressBar ProgressBar;
+    [Export] public Label HealthLabel;
+
+    [Export] public float BaseHealth = 100f;
+    [Export] public float HealthPerLevel = 25f;
+
+    private float _currentHealth = 100f;
+
+    public override void _Ready()
+    {
+        UpdateSize();
+    }
+
+    private void UpdateSize()
+    {
+        if (ProgressBar == null)
+        {
+            return;
+        }
+
+        float maxHealth = BaseHealth + (Level * HealthPerLevel);
+
+        ProgressBar.MaxValue = maxHealth;
+
+        _currentHealth = maxHealth;
+
+        ProgressBar.Value = _currentHealth;
+
+        if (HealthLabel != null)
+        {
+            HealthLabel.Text = $"{(int)_currentHealth} / {(int)maxHealth}";
+        }
+
+        GD.Print(maxHealth);
+    }
+
+}
