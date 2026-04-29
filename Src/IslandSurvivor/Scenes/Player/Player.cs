bd80ff163187019b15b0e7eb522f8d2ab01c34c2
Mason <antoine_masson94@hotmail.com>
2026-04-29 12:38:25 -0400

    ajout de hitmarker lorsqu'on frappe les ressources et les joueurs. Ajout de Y sorting pour les ennemis afin qu'ils passent en dessous des arbres. Corrections de fonction répétitives afin d'enlever la duplication de code

--- Src/IslandSurvivor/Scenes/Player/Player.cs
+++ Src/IslandSurvivor/Scenes/Player/Player.cs
@@ -32,6 +32,7 @@ public partial class Player : CharacterBody2D
 
     public override void _Ready()
     {
+        GD.Print("Attaque du joueur : " + Stats?.GetCurrentValue(StatType.Attack));
         m_interactionLabel.Visible = false;
 
         m_movementController = GetNodeOrNull<MovementController>("MovementController");
@@ -224,7 +225,7 @@ public partial class Player : CharacterBody2D
 
         m_hitTargetsThisAttack.Add(p_target);
 
-        int attackDamage = (int)(Stats?.GetCurrentValue(StatType.Attack) ?? 1000f); // Default to 10 if missing
+        int attackDamage = (int)(Stats?.GetCurrentValue(StatType.Attack) ?? 10f); // Default to 10 if missing
 
         GD.Print($"[COMBAT] Hit target! Dealing {attackDamage} damage.");
         p_target.TakeDamage(attackDamage, this);
