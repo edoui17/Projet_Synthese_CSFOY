a32f89cf6a065f29ced8b5106be9f6a1902f878f
Mason <antoine_masson94@hotmail.com>
2026-05-15 12:34:52 -0400

    ajout du suivi sur l'ajout des effets visuels, son sur le portail

--- Src/IslandSurvivor/Scenes/Portal/Portal.cs
+++ Src/IslandSurvivor/Scenes/Portal/Portal.cs
@@ -1,38 +1,3 @@
-//using Godot;
-//using System;
-//using Core.Domain.Models;
-//using IslandSurvivor.Interfaces;
-
-//public partial class Portal : Node2D
-//{
-//	private AnimationPlayer m_animationPlayer;
-//	private PortalInteraction m_interactionArea;
-
-//	public override void _Ready()
-//	{
-//		m_animationPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
-
-//		// The InteractionArea child has the script attached now
-//		m_interactionArea = GetNode<PortalInteraction>("InteractionArea");
-
-//		// Auto-activate the portal and point it to the Home Island if we are NOT in the PlayerHub.
-//		var currentScenePath = GetTree()?.CurrentScene?.SceneFilePath;
-//		if (currentScenePath != null && !currentScenePath.Contains("PlayerHub"))
-//		{
-//			ActivatePortal(IslandDestination.HomeIsland);
-//		}
-//	}
-
-//	public void ActivatePortal(IslandDestination p_destination)
-//	{
-//		m_animationPlayer.Play("ACTIVE");
-
-//		if (m_interactionArea != null)
-//		{
-//			m_interactionArea.SetDestination(p_destination);
-//		}
-//	}
-//}
 using Godot;
 using System;
 using Core.Domain.Models;
